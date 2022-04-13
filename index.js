d4040e17e8b9fdb1e79a0800809f90a86e938633
Eduardo Nishizuka <nishizuka23@gmail.com>
2022-04-12 21:58:30 -0300

    add ip address

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 
 const port = 3333
+const ip = require('ip').address()
 
 app.get('/', (req, res) => {
-  res.send('Blue 1.0.0')
+  res.send('Blue 1.0.0 - ' + ip)
 })
 
 app.listen(port, () => {
